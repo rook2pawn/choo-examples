c9cc2753e74e8aa0b2fb9bb35234ffd56fc996ff
rook2pawn <rook2pawn@gmail.com>
2018-04-06 05:54:25 -0700

    update

--- confirm-example/index.js
+++ confirm-example/index.js
@@ -1,48 +1,3 @@
-# choo examples!
-
-## Input
-This first one is an adapation from [here](http://requirebin.com/?gist=e589473373b3100a6ace29f7bbee3186).
-
-```javascript
-var html = require('choo/html')
-var choo = require('choo')
-
-var app = choo()
-app.use(titleStore)
-app.route('/', mainView)
-app.mount('body')
-
-function mainView (state, emit) {
-  return html`
-    <body>
-    <h1>${state.title}</h1>
-    <input
-      type="text"
-      value="${state.title}"
-      oninput=${oninput} />
-    </body>
-  `
-
-  function oninput (e) {
-    emit('update', e.target.value)
-  }
-}
-
-function titleStore (state, emitter) {
-  state.title = "Set the title"
-  emitter.on('update', function (title) {
-    state.title = title;
-    emitter.emit('render')
-  })
-}
-```
-
-
-## Confirm - Nested components
-
-The new Choo api really [cleans up](https://github.com/krawaller/comparison/tree/master/choo/src) nicely!
-
-```javascript
 var html = require("choo/html");
 var choo = require("choo");
 
@@ -117,5 +72,3 @@ app.use(confirmStore);
 app.use(submitStore);
 app.route("/", Submission);
 app.mount("body");
-
-```
