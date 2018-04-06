597a33351028e15a47115ce6ac54bfaabf1e085f
rook2pawn <rook2pawn@gmail.com>
2018-04-06 04:56:45 -0700

    update

--- input-example/index.js
+++ input-example/index.js
@@ -1,9 +1,3 @@
-# choo examples!
-
-## Input
-This first one is an adapation from [here](http://requirebin.com/?gist=e589473373b3100a6ace29f7bbee3186).
-
-```javascript
 var html = require('choo/html')
 var choo = require('choo')
 
@@ -35,7 +29,3 @@ function titleStore (state, emitter) {
     emitter.emit('render')
   })
 }
-```
-
-
-##
